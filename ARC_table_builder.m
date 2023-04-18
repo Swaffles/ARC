0be4c570ffc34e36e6141fcec04a83bb507d890e
Swaffles <58667766+Swaffles@users.noreply.github.com>
2023-04-18 16:49:21 -0500

    Modified flat table to contain 4 total sheets. sheet one is the total hydrodynamic forces, two is just the dynamic forces, 3 is the hydrostatic forces, and 4 contains all of the stdev for each value and where necessary across repeated trials. Outputs HydroData_FLAT.m which is a structured table called A where each field corresponds to the sheet data as described above and Hydrodata_Summary.xlxs which is the Excel file containing the four sheets. Has been tested. Next step is to work on the interpolation table and make sure it works correctly.

--- ARC_table_builder.m
+++ ARC_table_builder.m
@@ -40,18 +40,17 @@ catch
     programPath = uigetdir(pwd,'Select GitHub Folder');
     addpath(programPath);
 end
-try 
-    addpath(savePath)
-catch
-    savePath = uigetdir(homePath,'Select Folder where table should be saved');
-end
-
 try
     addpath(homePath)
     cd(homePath)
 catch
     homePath = uigetdir(pwd,'Select University of Iowa (1)\ARC 2022');
     cd(homePath)
+end
+try 
+    addpath(savePath)
+catch
+    savePath = uigetdir(homePath,'Select Folder where table should be saved');
 end
 
 list = {'Falt','Interpolation'};
@@ -79,117 +78,198 @@ switch TableMode
     
         % read in data from table frist. do some calcs too
         fields = fieldnames(Arc);
-        tableSize = [length(existingData), 23];
+        tableSize = [length(existingData), 22];
         varTypes = {'string','double','double','double','double','double',...
-            'double','double','double','double','double','double','double',...
-            'double','double','double','double','double','double','double',...
-            'double','double','double'};
-        varNames = {'Run Name','Mean Fx [N]','Std Fx [N]',...
-            'Mean Fy [N]','Std Fy [N]','Mean Fz [N]','Std Fz [N]',...
-            'Mean Mx [Nm]','Std Mx [Nm]','Mean My [Nm]','Std My [Nm]',...
-            'Mean Mz [Nm]','Std Mz [Nm]','Mean Water Depth [m]','Std Water Depth [m]',...
-            'Heading [deg]','Steering [deg]','Mean Flow Speed [m/s]',...
-            'Std Flow Speed [m/s]','FrH','Sr','Target Flow Speed [m/s]','H/D'};
+                    'double','double','double','double','double','double',...
+                    'double','double','double','double','double','double',...
+                    'double','double','double','double'};
+        varNames = {'Run Name','Fx [N]','Fy [N]','Fz [N]','Mx [Nm]',...
+                    'My [Nm]','Mz [Nm]','Wheel Fx [N]','Wheel Fy [N]',...
+                    'Wheel Fz [N]','Wheel Mx [N]','Wheel My [N]',...
+                    'Wheel Mz [N]','Water Depth [m]','Port Steering [deg]',...
+                    'Flow Speed [m/s]','Heading [deg]','FrH','Sr',...
+                    'Target Flow Speed [m/s]','H/D','WheelAngle'};
         yData = table('Size',tableSize,'VariableTypes',varTypes,'VariableNames',varNames);
-        for i=1:length(fields)
-            yData(i,1) = fields(i);
-            yData(i,2) = Arc.(fields{i})(1,2); % mean FX
-            yData(i,3) = Arc.(fields{i})(1,5); % stdev Fx
-            yData(i,4) = Arc.(fields{i})(2,2); % mean Fy
-            yData(i,5) = Arc.(fields{i})(2,5); % stdev Fy
-            yData(i,6) = Arc.(fields{i})(3,2); % mean Fz
-            yData(i,7) = Arc.(fields{i})(3,5); % stdev Fz
-            yData(i,8) = Arc.(fields{i})(4,2); % mean Mx
-            yData(i,9) = Arc.(fields{i})(4,5); % stdev Mx
-            yData(i,10) = Arc.(fields{i})(5,2); % mean My
-            yData(i,11) = Arc.(fields{i})(5,5); % stdev My
-            yData(i,12) = Arc.(fields{i})(6,2); % mean Mz
-            yData(i,13) = Arc.(fields{i})(6,2); % stdev Mz
-            % other parameters
-            temp1 = Arc.(fields{i}){13,2}/100; % mean water depth [m]
-            yData(i,14) = {temp1};
-            temp2 = Arc.(fields{i}){13,5}/100; % stdev water depth [m]
-            yData(i,15) = {temp2};
-            yData(i,16) = Arc.(fields{i})(15,2); % heading
-            yData(i,17) = Arc.(fields{i})(16,2); % steering
-            temp3 = Arc.(fields{i}){17,2}/100; % mean flow speed [m/s]
-            yData(i,18) = {temp3};
-            temp4 = Arc.(fields{i}){17,5}/100; % stdev flow speed [m/s]
-            yData(i,19) = {temp4};
-
-            % flow speed
-            FrH = yData{i,18}/(sqrt(gravity*yData{i,14}));
-            Sr = yData{i,18}/(length_Scale^1/3);
-            FrHSr = FrH*sqrt(Sr);
-            yData(i,20) = {round(FrHSr,2,"significant")};
-            yData(i,21) = {round(Sr,2,"decimals")};
-            yData(i,22) = Arc.(fields{i})(18,2); % target flow speed
-            yData(i,23) = Arc.(fields{i})(19,2); % h/D
-            
-        end
+        A = struct;
+        for j = 1:4
+            if j == 1
+                item = 2; % select total
+                yData.Properties.VariableNames = {'Run Name','Total Fx [N]','Total Fy [N]',...
+                    'Total Fz [N]','Total Mx [Nm]','Total My [Nm]',...
+                    'Total Mz [Nm]','Total Wheel Fx [N]','Total Wheel Fy [N]',...
+                    'Total Wheel Fz [N]','Total Wheel Mx [N]','Total Wheel My [N]',...
+                    'Total Wheel Mz [N]','Water Depth [m]','Port Steering [deg]',...
+                    'Flow Speed [m/s]','Heading [deg]','FrH','Sr',...
+                    'Target Flow Speed [m/s]','H/D','WheelAngle'};
+            elseif j == 2
+                item = 3; % select dynamic
+                yData.Properties.VariableNames = {'Run Name','Dynamic Fx [N]','Dynamic Fy [N]',...
+                    'Dynamic Fz [N]','Dynamic Mx [Nm]','Dynamic My [Nm]',...
+                    'Dynamic Mz [Nm]','Dynamic Wheel Fx [N]','Dynamic Wheel Fy [N]',...
+                    'Dynamic Wheel Fz [N]','Dynamic Wheel Mx [N]','Dynamic Wheel My [N]',...
+                    'Dynamic Wheel Mz [N]','Water Depth [m]','Port Steering [deg]',...
+                    'Flow Speed [m/s]','Heading [deg]','FrH','Sr',...
+                    'Target Flow Speed [m/s]','H/D','WheelAngle'};
+            elseif j == 3
+                item = 4; % select hydrostatic
+                yData.Properties.VariableNames = {'Run Name','Hydrostatic Fx [N]','Hydrostatic Fy [N]',...
+                    'Hydrostatic Fz [N]','Hydrostatic Mx [Nm]','Hydrostatic My [Nm]',...
+                    'Hydrostatic Mz [Nm]','Hydrostatic Wheel Fx [N]','Hydrostatic Wheel Fy [N]',...
+                    'Hydrostatic Wheel Fz [N]','Hydrostatic Wheel Mx [N]','Hydrostatic Wheel My [N]',...
+                    'Hydrostatic Wheel Mz [N]','Water Depth [m]','Port Steering [deg]',...
+                    'Flow Speed [m/s]','Heading [deg]','FrH','Sr',...
+                    'Target Flow Speed [m/s]','H/D','WheelAngle'};
+            else
+                item = 5; % select stdev
+                yData.Properties.VariableNames = {'Run Name','STDEV Fx [N]','STDEV Fy [N]',...
+                    'STDEV Fz [N]','STDEV Mx [Nm]','STDEV My [Nm]',...
+                    'STDEV Mz [Nm]','STDEV Wheel Fx [N]','STDEV Wheel Fy [N]',...
+                    'STDEV Wheel Fz [N]','STDEV Wheel Mx [N]','STDEV Wheel My [N]',...
+                    'STDEV Wheel Mz [N]','Water Depth [m]','STDEV Port Steering [deg]',...
+                    'Flow Speed [m/s]','Heading [deg]','FrH','Sr',...
+                    'Target Flow Speed [m/s]','H/D','WheelAngle'};
+            end
+            for i=1:length(fields)
+                yData(i,1) = fields(i);
+                yData(i,2) = Arc.(fields{i})(1,item); % FX
+                yData(i,3) = Arc.(fields{i})(2,item); % Fy
+                yData(i,4) = Arc.(fields{i})(3,item); % Fz
+                yData(i,5) = Arc.(fields{i})(4,item); % Mx
+                yData(i,6) = Arc.(fields{i})(5,item); % My
+                yData(i,7) = Arc.(fields{i})(6,item); % Mz
+                yData(i,8) = Arc.(fields{i})(7,item); % Wheel Fx
+                yData(i,9) = Arc.(fields{i})(8,item); % Wheel Fy
+                yData(i,10) = Arc.(fields{i})(9,item); % Wheel Fz 
+                yData(i,11) = Arc.(fields{i})(10,item); % Wheel Mx
+                yData(i,12) = Arc.(fields{i})(11,item); % Wheel My
+                yData(i,13) = Arc.(fields{i})(12,item); % Wheel Mz
+                % other parameters
+                temp1 = Arc.(fields{i}){13,item}/100; % mean water depth [m]
+                yData(i,14) = {temp1};
+                yData(i,15) = Arc.(fields{i})(14,item); % Port steering
+                temp3 = Arc.(fields{i}){17,item}/100; % mean flow speed [m/s]
+                yData(i,16) = {temp3};
+                yData(i,17) = Arc.(fields{i})(15,2); % heading
+                % non dimensional
+                FrH = yData{i,16}/(sqrt(gravity*yData{i,14})); % V/sqrt(g*depth)
+                Sr = yData{i,14}/(length_Scale^1/3); % depth/Vol^1/3
+                FrHSr = FrH*sqrt(Sr);
+                yData(i,18) = {round(FrHSr,2,"significant")};
+                yData(i,19) = {round(Sr,2,"decimals")};
+                % internal sorting use
+                yData(i,20) = Arc.(fields{i})(18,2); % target flow speed
+                yData(i,21) = Arc.(fields{i})(19,2); % h/D
+                yData(i,22) = Arc.(fields{i})(16,2); % steering
+            end % i for loop
+            if j == 1
+                A.Total = yData;
+            elseif j == 2
+                A.Dynamic = yData;
+            elseif j == 3
+                A.Hydrostatic = yData;
+            else
+                A.STDEV = yData;
+            end
+        end %j for loop
         fprintf("YData table created!\n");
         fprintf("Subsorting and reducing repeats\n")
         % Extract the repeats in the table and recalculate the mean and 
         % standard deviation for those conditions. Uses the heading, target
         % flow speed, and H/D to find the indecies
-        depths = unique(yData{:,"H/D"});
-        count = 1;
-        for i=1:length(depths)
-            indD = yData{:,"H/D"} == depths(i); 
-            yData1{i} = yData(indD,:);
-            speeds = unique(yData1{i}{:,"Target Flow Speed [m/s]"});
-            for j = 1:length(speeds)
-                indS = yData1{i}{:,"Target Flow Speed [m/s]"} == speeds(j);
-                yData2{j} = yData1{i}(indS,:);
-                steering = unique(yData2{j}{:,"Steering [deg]"});
-                for k = 1:length(steering)
-                    indSt = yData2{j}{:,"Steering [deg]"} == steering(k);
-                    yData3{count} = yData2{j}(indSt,:);
-                    % for repeat headings we need to average the values
-                    % across all the rows
-                    [GC,GR] = groupcounts(yData3{count}{:,"Heading [deg]"});
-                    if any(GC>1)
-                        % there are repeats, identify the value
-                        indGC = find(GC>1);
-                        valueGR = GR(indGC); % this may be larger than 1 value
-                        % find corresponding row from yData3
-                        for a = 1:length(valueGR)
-                            rowsYData3 = find(yData3{count}{:,"Heading [deg]"}==valueGR(a));
-                            temp = yData3{count}(rowsYData3,:);
-                            yData3{count}(rowsYData3,:) = []; % deletes the rows
-                            % average all the cols
-                            mu = mean(temp{:,2:end},1); % 2:end ignores the string
-                            % find the original run name that was
-                            % replicated or repeated
-                            %name = strfind(temp{:,1},"EFR");
-                            name = 1; % original EF always first
-                            newYData3row = temp(1,:);
-                            newYData3row{1,1} = temp{name,1}; %should write to the correct place
-                            newYData3row{1,2:end} = mu;
-                            yData3{count}(end+1,:) = newYData3row;
+        for indx = 1:4
+            if indx == 1
+                yData = A.Total;
+            elseif indx == 2
+                yData = A.Dynamic;
+            elseif indx == 3
+                yData = A.Hydrostatic;
+            else
+                 yData = A.STDEV;
+            end
+            depths = unique(yData{:,"H/D"});
+            count = 1;
+            for i=1:length(depths)
+                indD = yData{:,"H/D"} == depths(i); 
+                yData1{i} = yData(indD,:);
+                speeds = unique(yData1{i}{:,"Target Flow Speed [m/s]"});
+                for j = 1:length(speeds)
+                    indS = yData1{i}{:,"Target Flow Speed [m/s]"} == speeds(j);
+                    yData2{j} = yData1{i}(indS,:);
+                    steering = unique(yData2{j}{:,"WheelAngle"});
+                    for k = 1:length(steering)
+                        indSt = yData2{j}{:,"WheelAngle"} == steering(k);
+                        yData3{count} = yData2{j}(indSt,:);
+                        % for repeat headings we need to average the values
+                        % across all the rows
+                        [GC,GR] = groupcounts(yData3{count}{:,"Heading [deg]"});
+                        if any(GC>1)
+                            % there are repeats, identify the value
+                            indGC = find(GC>1);
+                            valueGR = GR(indGC); % this may be larger than 1 value
+                            % find corresponding row from yData3
+                            for a = 1:length(valueGR)
+                                rowsYData3 = find(yData3{count}{:,"Heading [deg]"}==valueGR(a));
+                                temp = yData3{count}(rowsYData3,:);
+                                yData3{count}(rowsYData3,:) = []; % deletes the rows
+                                if indx == 4
+                                    mu = rms(temp{:,2:end},1);
+                                else
+                                    % average all the cols
+                                    mu = mean(temp{:,2:end},1); % 2:end ignores the string, 1 works on cols
+                                end
+                                % find the original run name that was
+                                % replicated or repeated
+                                %name = strfind(temp{:,1},"EFR");
+                                name = 1; % original EF always first
+                                newYData3row = temp(1,:);
+                                newYData3row{1,1} = temp{name,1}; %should write to the correct place
+                                newYData3row{1,2:end} = mu;
+                                yData3{count}(end+1,:) = newYData3row;
+                            end
+                            yData3{count} = sortrows(yData3{count},'Heading [deg]');
                         end
-                        yData3{count} = sortrows(yData3{count},'Heading [deg]');
-                    end
-                    count = count + 1;
-                end % k for loop
-            end % j for loop
-        end % i for loop
-        fprintf("Concatenating yData3 tables\n");
-        yData(:,:) = []; %deletes values in array
-        yData = yData3{1};
-        for i = 2:length(yData3) 
-            yData = [yData;yData3{i}]; 
-        end
-        yData = sortrows(yData,'Run Name');
-        save("HydroData_FLAT","yData",'-v7.3');
-        fprintf("YData successfully saved to %s as HydroData_FLAT.m\n",pwd);
-        clearvars -except homePath dataPath programPath Arc yData savePath
+                        count = count + 1;
+                    end % k for loop
+                end % j for loop
+            end % i for loop
+            fprintf("Concatenating yData3 tables\n");
+            yData(:,:) = []; %deletes values in array
+            yData = yData3{1};
+            for i = 2:length(yData3) 
+                yData = [yData;yData3{i}]; 
+            end
+            yData = sortrows(yData,'Run Name');
+            if indx == 1
+                A.Total = yData;
+            elseif indx == 2
+                A.Dynamic = yData;
+            elseif indx == 3
+                A.Hydrostatic = yData;
+            else
+                A.STDEV = yData;
+            end
+            clear yData1 yData2 yData3 %must clear for correct loop function
+        end % indx outer loop
+        save("HydroData_FLAT","A",'-v7.3');
+        fprintf("Data successfully saved to %s as HydroData_FLAT.m\n",pwd);
+        fprintf("Writing Data to Excel file\n");
+        sheet = 'Total';
+        writetable(A.Total,"Hydrodata_Summary.xlsx",'Sheet',sheet,'Range','A1');
+        sheet = 'Dynamic';
+        writetable(A.Dynamic,"Hydrodata_Summary.xlsx",'Sheet',sheet,'Range','A1');
+        sheet = 'Hydrostatic';
+        writetable(A.Hydrostatic,"Hydrodata_Summary.xlsx",'Sheet',sheet,'Range','A1');
+        sheet = 'STDEV';
+        writetable(A.STDEV,"Hydrodata_Summary.xlsx",'Sheet',sheet,'Range','A1');
+        fprintf("Successfully created Hydrodata_Summary.xlxs in %s\n",pwd);
+        clearvars -except homePath dataPath programPath Arc yData savePath A
 
     case 2
         % interpolation table
         
         % read in values from flat table
         load HydroData_FLAT.mat %loads in as yData
+        yData = A.Total; %just want to operate on the totals
         Uvel = unique(yData{:,"Mean Flow Speed [m/s]"});
         Nu = length(Uvel);
         Depth = unique(yData{:,"Mean Water Depth [m]"});
