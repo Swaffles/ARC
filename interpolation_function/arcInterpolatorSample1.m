796c11d92a6cd5bb98117eb57d9bbee67aa24f66
Mike Swafford <58667766+Swaffles@users.noreply.github.com>
2023-05-05 14:20:37 -0500

    Some organization didn't sync
    last commit had an internet connection interruption so recommitting to make sure things are correct.

--- interpolation_function/arcInterpolatorSample1.m
+++
@@ -1,8 +0,0 @@
-% Sample script to demonstrate execution of function Vq = arcInterpolator(steering2Interp, heading2Interp, depth2Interp, speed2Interp)
-steering2Interp = 0; % Initialize steering2Interp here
-heading2Interp = 0; % Initialize heading2Interp here
-depth2Interp = 0.1; % Initialize depth2Interp here
-speed2Interp = 0.3; % Initialize speed2Interp here
-disp([steering2Interp,heading2Interp,depth2Interp,speed2Interp]);
-Vq = arcInterpolator(steering2Interp, heading2Interp, depth2Interp, speed2Interp);
-disp(Vq);
